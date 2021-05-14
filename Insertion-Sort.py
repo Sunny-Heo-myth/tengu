4e15e36ef8f758a8994f275e2d7d83b2758df351
Astronaut <hsymyth@gmail.com>
2021-05-14 23:57:26 +0900

    rgexp

---
+++ Insertion-Sort.py
@@ -0,0 +1,116 @@
+# Insertion sort
+
+def insertion_sort(arr) :
+    
+    for i in range(1, len(arr)) :
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key :
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+x = [1,5,4,7,50,15,3,35,72,9]
+insertion_sort(x)
+print(x)
+
+# Reverse
+
+def insertion_sort_reverse(arr) :
+    
+    for i in range(1, len(arr)) :
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < key :
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+x = [1,5,4,7,50,15,3,35,72,9]
+insertion_sort_reverse(x)
+print(x)
+
+# Linear search
+
+def search(arr, x) :
+    for i in arr :
+        if arr[i] == x :
+            return i
+            break
+        else :
+            return "NIL"
+
+# Binary addition
+
+def binary_addition(arr1, arr2) :
+    arr1.insert(0, 0)
+    arr2.insert(0, 0)
+    arr = [0] * len(arr1)
+    plus = 0
+
+    for i in reversed(range(len(arr1))) :
+        if plus + arr1[i] + arr2[i] == 0 : arr[i] = 0; plus = 0
+        elif plus + arr1[i] + arr2[i] == 1 : arr[i] = 1; plus = 0
+        elif plus + arr1[i] + arr2[i] == 2 : arr[i] = 0; plus = 1
+        else : arr[i] = 1 ; plus = 1
+
+    return arr
+
+arr1 = [1,0,0,0,0,1]
+arr2 = [1,0,0,0,0,1]
+print(binary_addition(arr1, arr2))
+
+# Selection sort
+
+def selection_sort(arr) :
+    for i in range(len(arr)):
+
+        for j in range(i+1, len(arr)):
+
+            if arr[i] > arr[j] :
+                arr.insert(0, arr[j])
+                del arr[j+1]
+
+arr = [5,44,3,52,331]
+selection_sort(arr)
+print(arr)
+        
+# Merge sort
+
+def merge_sort(arr):
+    
+    if len(arr)>1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+        
+        i=j=k=0       
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k]=L[i]
+                i += 1
+            else:
+                arr[k]=R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k]=L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k]=R[j]
+            j += 1
+            k += 1
+    
+x = [1,8,5,34,6,87,6,4,3,6,8]
+merge_sort(x)
+print(x)
---
+++ do_it_exercise.py
@@ -0,0 +1,269 @@
+# Q1
+print("<<Q1>>")
+
+x = 'a:b:c:d'
+print('#'.join(x.split(':')))
+
+# Q2
+print("<<Q2>>")
+
+a = {'A': 90, 'B':80}
+a['C'] = 70
+print(a['C'])
+
+# Q3
+print("<<Q3>>")
+
+a = [1,2,3]
+print(id(a))
+a += [4, 5]
+print(id(a))
+a = a + [4, 5]
+print(id(a))
+
+b = [1,2,3]
+print(id(b))
+b.extend([4, 5])
+print(id(b))
+
+# Q4
+print("<<Q4>>")
+
+A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25] ; y = 0
+while A :
+    a = A.pop()
+    if a >= 50 : y += a
+
+print(y)
+
+# Q5
+print("<<Q5>>")
+
+def pibo(x) :
+    arr = [0, 1]
+    while arr[-1] <= x :
+        arr.append(arr[-2] + arr[-1])
+    del arr[-1]
+    return arr
+
+print(pibo(15))
+
+# Q6
+print("<<Q6>>")
+
+x = map(int, input().split(','))
+y = 0
+for i in x : y += i
+print(y)
+
+#Q7
+print("<<Q7>>")
+
+print('Enter number : ', end = ' ')
+x = int(input())
+for i in range(1, 10) :
+    print(x*i, end = ' ')
+print()
+
+#Q8
+print("<<Q8>>")
+
+f_r = open("abc.txt", "r")
+lines = f_r.readlines()
+print(lines)
+f_r.close()
+
+
+f_w = open("abc.txt", "w")
+for i in reversed(range(0, len(lines))) :
+    f_w.write(lines[i])
+f_w.close()
+
+f_r = open("abc.txt", "r")
+while True :
+    line = f_r.readline()
+    if not line : break
+    print(line)
+
+#Q9
+print("<<Q9>>")
+
+f_r = open('sample.txt', 'r')
+arr = []
+while True :
+    line = f_r.readline()
+    if not line : break
+    arr.append(line)
+f_r.close()
+
+sum = 0
+for i in arr :
+    sum += int(i)
+average = str(sum/len(arr))
+
+f_w = open('result.txt', 'w')
+f_w.write(average)
+f_w.close()
+
+#Q10
+print("<<Q10>>")
+
+class Calculator() :
+    def __init__(self, arr) :
+        self.arr = arr
+        
+    def setdata(self, arr) :
+        self.arr = arr
+
+    def sum(self) :
+        y = 0
+        for i in self.arr : y += i
+        return y
+
+    def avg(self) :
+        return sum(self.arr)/len(self.arr)
+
+cal1 = Calculator([1,2,3,4,5])
+cal2 = Calculator([6,7,8,9,10])
+print(cal1.sum())
+print(cal1.avg())
+print(cal2.sum())
+print(cal2.avg())
+
+#Q11
+print("<<Q11>>")
+# import module
+# from module import function
+# from module import function1, function2
+# from module import*
+#import sys
+# sys.path.append("C:/.../module")
+# import module
+# set PYTHONPATH="C:/.../folder"
+# import module
+
+#Q12
+print("<<Q12>>")
+
+result = 0
+try :
+    [1,2,3][3]
+    "a" + 1
+    4/0
+except TypeError:
+    result += 1
+except ZeroDivisionError:
+    result += 2
+except IndexError:
+    result += 3
+finally:
+    result += 4
+
+print(result)
+
+#Q13
+print("<<Q13>>")
+
+def DashInsert(x):
+    k = 0
+    for i in range(len(x)-1):
+        x = list(x)
+        a1 = int(x[i+k]) ; a2 = int(x[i+k+1])
+        if a1%2 == 0 and a2%2 == 0:
+            x.insert(i+k+1, '*')
+            k += 1
+        elif a1%2 == 1 and a2%2 == 1:
+            x.insert(i+k+1, '-')
+            k += 1
+
+    y = ''
+    for i in x :
+        y += i
+
+    return y
+
+x = '4546793'
+print(DashInsert(x))
+
+#Q14
+print("<<Q14>>")
+
+def condense(x) :
+    x += '0'
+    arr = []
+    k = 1
+    for i in range(len(x)-1) :
+        if x[i] == x[i+1] :
+            k += 1
+        else :
+            arr.append(x[i])
+            arr.append(str(k))
+            k = 1
+
+    y = ''
+    for i in arr :
+        y += i
+    return y
+
+print(condense('aaabbcccccca'))
+
+#Q15
+print("<<Q15>>")
+
+def dupli_find(x) :
+    x = list(str(x))
+    for i in range(len(x)) :
+        a = x.pop(i)
+        if a in x :
+            return False
+        else :
+            return True
+
+print(dupli_find('012345679'))
+        
+#Q16
+print("<<Q16>>")
+
+def morse(x) :
+    morse = {'.-' : 'A',
+             '-...' : 'B',
+             '-.-.' : 'C',
+             '-..' : 'D',
+             '.' : 'E',
+             '..-.' : 'F',
+             '--.' : 'G',
+             '....' : 'H',
+             '..' : 'I',
+             '.---' : 'J',
+             '-.-' : 'K',
+             '.-..' : 'L',
+             '--' : 'M',
+             '-.' : 'N',
+             '---' : 'O',
+             '.--.' : 'P',
+             '--.-' : 'Q',
+             '.-.' : 'R',
+             '...' : 'S',
+             '-' : 'T',
+             '..-' : 'U',
+             '...-' : 'V',
+             '.--' : 'W',
+             '-..-' : 'X',
+             '-.--' : 'Y',
+             '--..' : 'Z',
+             '' : ' '}
+    x = x.split(' ')
+    a = []
+    for i in x:
+        a.append(morse.get(i))
+    y =''
+    for i in a :
+        y += i
+    return y
+
+print(morse('.... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--'))
+
+#Q17
+print("<<Q17>>")
+
+
